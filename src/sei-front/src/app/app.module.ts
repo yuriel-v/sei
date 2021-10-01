8d0fc40566027274a3cdad57bc06d24a945c592e
yuriel-v <emberbec@gmail.com>
2021-10-01 17:00:03 -0300

    Add skeleton of login component

--- src/sei-front/src/app/app.module.ts
+++ src/sei-front/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     NgbModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    LoginModule
   ],
   providers: [Title],
   bootstrap: [AppComponent]
