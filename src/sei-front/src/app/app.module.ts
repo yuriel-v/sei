957c0d952425ddc99a3591f0019bf9f607865ac4
Gabriel Fidelis <gabrielfidsouza@gmail.com>
2021-10-01 16:59:35 -0300

    [feature] Initial home page

--- src/sei-front/src/app/app.module.ts
+++ src/sei-front/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     NgbModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    HomeModule
   ],
   providers: [Title],
   bootstrap: [AppComponent]
