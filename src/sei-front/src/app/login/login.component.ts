9cb73a817a9b64ca017fb111791aea8e21a1fd47
Roger <roger.pbernardino@gmail.com>
2021-10-09 00:10:40 -0300

    Validate e-mail

--- src/sei-front/src/app/login/login.component.ts
+++ src/sei-front/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -12,5 +14,13 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Insira um endereço';
+    }
+
+    return this.email.hasError('email') ? 'Endereço Inválido' : '';
+  }
 }
