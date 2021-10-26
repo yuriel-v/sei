c28bdd5ada7db9686180d8fcfc68d5c75698ce44
Gabriel Fidelis <gabrielfidsouza@gmail.com>
2021-10-26 18:04:48 -0300

    [rebase] Rebasing uni-skeleton to backend-db

--- src/sei-front/src/app/shared/header/header.component.ts
+++ src/sei-front/src/app/shared/header/header.component.ts
@@ -28,6 +28,9 @@ export class HeaderComponent implements OnInit {
     this.sidebarOpened = !this.sidebarOpened;
   }
 
+  testGet() { 
+    this.testService.getTest().subscribe();
+  }
   ngOnInit(): void {
     if (this.authService.isLogged()) { 
       this.isLogged = true;
