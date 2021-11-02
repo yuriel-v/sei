0553abfde45a9608bc6f5174775ce272ba843104
kvm <kvm@vm002>
2021-11-02 12:19:37 -0300

    Update README, add JAX-RS, create endpoint class
    - Also comment out index servlet's content

---
+++ src/sei-back/src/main/java/io/sei/api/Endpoint.java
@@ -0,0 +1,5 @@
+package io.sei.api;
+
+public class Endpoint {
+    
+}
--- src/sei-back/src/main/java/io/sei/backend/IndexServlet.java
+++ src/sei-back/src/main/java/io/sei/backend/IndexServlet.java
@@ -1,43 +1,45 @@
-package io.sei.backend;
+// Commented out for historical purposes
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
+// package io.sei.backend;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+// import java.io.IOException;
+// import java.io.PrintWriter;
+// import java.util.HashMap;
+// import java.util.Map;
 
-import com.google.gson.Gson;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+
+// import com.google.gson.Gson;
 
 
-@WebServlet(name = "IndexServlet", urlPatterns = {""})
-public class IndexServlet extends HttpServlet 
-{
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
-    {
-        // more "complete" version
-        // showcases hashmap and gson's toJson() conversion
-        PrintWriter out = response.getWriter();
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+// @WebServlet(name = "IndexServlet", urlPatterns = {""})
+// public class IndexServlet extends HttpServlet 
+// {
+//     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+//     {
+//         // more "complete" version
+//         // showcases hashmap and gson's toJson() conversion
+//         PrintWriter out = response.getWriter();
+//         response.setContentType("application/json");
+//         response.setCharacterEncoding("UTF-8");
 
-        Map<String, String> jsonResponse = new HashMap<String, String>();
-        jsonResponse.put("response", "Index resource servlet!");
-        new Gson().toJson(jsonResponse, out);
-        out.flush();
-    }
+//         Map<String, String> jsonResponse = new HashMap<String, String>();
+//         jsonResponse.put("response", "Index resource servlet!");
+//         new Gson().toJson(jsonResponse, out);
+//         out.flush();
+//     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
-    {
-        // simpler version
-        PrintWriter out = response.getWriter();
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+//     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+//     {
+//         // simpler version
+//         PrintWriter out = response.getWriter();
+//         response.setContentType("application/json");
+//         response.setCharacterEncoding("UTF-8");
 
-        out.print("{\"response\": \"Index resource servlet!\"}");
-        out.flush();
-    }
-}
+//         out.print("{\"response\": \"Index resource servlet!\"}");
+//         out.flush();
+//     }
+// }
