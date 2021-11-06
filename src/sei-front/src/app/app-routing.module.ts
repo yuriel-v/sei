8d733c3955ae3d8d41c99b9708e46d6eb1239d73
Gabriel Fidelis <gabrielfidsouza@gmail.com>
2021-11-05 22:12:18 -0300

    [feature] Auth guard + changing 'login' route to 'auth'

--- src/sei-front/src/app/app-routing.module.ts
+++ src/sei-front/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './guards/auth.guard';
 import { AboutComponent } from './home/about/about.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'', component:AboutComponent},
-  {path: "login", component: LoginComponent}
+  {path:'', component:AboutComponent}
 ];
 
 @NgModule({
