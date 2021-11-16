4f2ac1ca93f182c850e7519fb772d594129a723b
yuriel-v <emberbec@gmail.com>
2021-11-16 20:49:20 -0300

    Change /agenda GET json format
    - Add expiration date, average and situation methods to enrollment
    - Add GET body builder (kindly named after a famous bodybuilder)

--- src/sei-back/src/main/java/io/sei/db/model/Enrollment.java
+++ src/sei-back/src/main/java/io/sei/db/model/Enrollment.java
@@ -1,5 +1,6 @@
 package io.sei.db.model;
 
+import java.text.SimpleDateFormat;
 import java.time.YearMonth;
 import java.time.ZoneId;
 import java.util.ArrayList;
@@ -70,6 +71,18 @@ public class Enrollment
         return currentSemester > registrationSemester;
     }
 
+    public Date getExpiration()
+    {
+        int curYear = YearMonth.now().getYear();
+        int expMonth = YearMonth.now().getMonthValue() < 7 ? 6 : 12;
+        try {
+            return new SimpleDateFormat("dd/MM/yyyy").parse(String.format("31/%d/%d", expMonth, curYear));
+        }
+        catch (Exception e) {  // should never happen, but just so the compiler doesn't bitch about "uncaught exception"
+            return null;
+        }
+    }
+
     public boolean isLocked() {
         return this.locked || this.isExpired();
     }
@@ -81,4 +94,41 @@ public class Enrollment
         }
         return null;
     }
+
+    public boolean isOngoing()
+    {
+        boolean av1Done = this.findExam(ExamType.AV1).getStatus() == ExamStatus.OK || this.findExam(ExamType.APS1).getStatus() == ExamStatus.OK;
+        boolean av2Done = this.findExam(ExamType.AV2).getStatus() == ExamStatus.OK || this.findExam(ExamType.APS2).getStatus() == ExamStatus.OK;
+        boolean av3Done = this.findExam(ExamType.AV3).getStatus() == ExamStatus.OK;
+
+        return !((av1Done && (av2Done || av3Done)) || (av2Done && av3Done));
+    }
+
+    public double getAverage()
+    {
+        double[] grades = {
+            this.findExam(ExamType.AV1).getGrade() + this.findExam(ExamType.APS1).getGrade(),
+            this.findExam(ExamType.AV2).getGrade() + this.findExam(ExamType.APS2).getGrade(),
+            this.findExam(ExamType.AV3).getGrade()
+        };
+
+        double lowest = grades[0];
+        for (double grade : grades)
+        {
+            if (grade < lowest) {
+                lowest = grade;
+            }
+        }
+
+        return (grades[0] + grades[1] + grades[2] - lowest) / 2;
+    }
+
+    public double getFinalGrade() {
+        return this.isOngoing() ? -1.0 : this.getAverage();
+    }
+
+    public String getSituation() {
+        return this.isOngoing() ? "Em andamento" : (this.getAverage() < 7 ? "Reprovado" : "Aprovado");
+    }
+
 }
